f87deeb191e71b3c5223dbb3ac26515c1645e950
vikasvikky12345 <rebbavikas2000@gmail.com>
2024-08-28 18:08:25 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,20 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { default: mongoose } = require("mongoose");
+mongoose
+  .connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
 const port = 8000;
 const app = express();
 app.get("/", (req, res) => res.send("hello vikas"));
